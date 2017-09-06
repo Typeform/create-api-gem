5600edbf8f7240e2133fd5b5af4ef111f0efe148
simondobson <simon.dobson@typeform.com>
2017-09-06 10:15:20 +0200

    Move token into env variable

--- test/test_forms.rb
+++ test/test_forms.rb
@@ -3,21 +3,25 @@ require 'create_api_gem'
 
 class FormsTest < Minitest::Test
 
+    def token
+        ENV['TYPEFORM_API_TOKEN']
+    end
+
     def test_crud
         form = Form.full_example
-        create_form = CreateFormRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', form)
+        create_form = CreateFormRequest.new(token, form)
         assert_equal create_form.success?, true
         form = create_form.form
 
-        retrieve_form = RetrieveFormRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', form)
+        retrieve_form = RetrieveFormRequest.new(token, form)
         assert_equal retrieve_form.success?, true
         form = retrieve_form.form
 
-        update_form = UpdateFormRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', form)
+        update_form = UpdateFormRequest.new(token, form)
         assert_equal update_form.success?, true
         form = update_form.form
 
-        delete_form = DeleteFormRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', form)
+        delete_form = DeleteFormRequest.new(token, form)
         assert_equal delete_form.success?, true
     end
 
--- test/test_themes.rb
+++ test/test_themes.rb
@@ -3,21 +3,25 @@ require 'create_api_gem'
 
 class ThemesTest < Minitest::Test
 
+    def token
+        ENV['TYPEFORM_API_TOKEN']
+    end
+
     def test_crud
         theme = Theme.full_example
-        create_theme = CreateThemeRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', theme)
+        create_theme = CreateThemeRequest.new(token, theme)
         assert_equal create_theme.success?, true
         theme = create_theme.theme
 
-        retrieve_theme = RetrieveThemeRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', theme)
+        retrieve_theme = RetrieveThemeRequest.new(token, theme)
         assert_equal retrieve_theme.success?, true
         theme = retrieve_theme.theme
 
-        update_theme = UpdateThemeRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', theme)
+        update_theme = UpdateThemeRequest.new(token, theme)
         assert_equal update_theme.success?, true
         theme = update_theme.theme
 
-        delete_theme = DeleteThemeRequest.new('DHDPHdKJjJxSjv6ORG98upmUb_Ayupq5kjgNXbGAlF4=', theme)
+        delete_theme = DeleteThemeRequest.new(token, theme)
         assert_equal delete_theme.success?, true
     end
 
