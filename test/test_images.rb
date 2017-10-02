2d858b117b674a2f4df9de349495d9fb6d633b2e
simondobson <simon.dobson@typeform.com>
2017-10-02 17:04:54 +0200

    Remaining images tests

--- test/test_images.rb
+++ test/test_images.rb
@@ -15,13 +15,23 @@ class ImagesTest < TestBase
     assert_equal image.same?(retrieve_image.image), true
     image = retrieve_image.image
 
+    retrieve_frame = RetrieveFrameRequest.new(image, 'first')
+    assert_equal retrieve_frame.success?, true
+
+    retrieve_all_images = RetrieveAllImagesRequest.new(token)
+    assert_equal retrieve_all_images.success?, true
+
     delete_image = DeleteImageRequest.new(token, image)
     assert_equal delete_image.success?, true
   end
 
-  def test_same_method; end
+  def test_same_method
+    image = Image.full_example
+    same_image = image.dup
+    assert_equal image.same?(same_image), true
 
-  def test_retrieve_all_images_request; end
-
-  def test_retrieve_frame_request; end
+    different_image = image.dup
+    different_image.file_name = 'different_file_name'
+    assert_equal image.same?(different_image), false
+  end
 end
