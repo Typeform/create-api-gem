4e09d5b68db36d7d7904a4f7084347a63b194f4e
simondobson <simon.dobson@typeform.com>
2017-10-02 17:36:06 +0200

    Remaining teams tests

--- test/test_teams.rb
+++ test/test_teams.rb
@@ -2,7 +2,15 @@ require 'minitest/autorun'
 require 'create_api_gem'
 
 class WorkspacesTest < TestBase
-  def test_crud_operations; end
+  def test_crud_operations
+    retrieve_team = RetreiveTeamRequest.new(token)
+    assert retrieve_team.success?, true
 
-  def test_same_method; end
+    default_workspace = RetrieveAllWorkspacesRequest.execute(token).default_workspace
+    email = 'maria_jose@typeform.com'
+    PatchWorkspaceRequest.execute(token, default_workspace, [PatchWorkspaceOperation.new(op: 'add', path: '/members', value: email)])
+
+    update_team = UpdateTeamRequest.new(token, [PatchTeamOperation.new(op: 'remove', path: '/members', value: email)])
+    assert update_team.success?, true
+  end
 end
