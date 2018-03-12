d0b09f6529ce78b3b8ad821db97c0772d2fec919
simondobson <simon.dobson@typeform.com>
2018-03-12 15:21:50 +0100

    Add from_id as parameter for get all forms

--- lib/create_api_gem/forms/requests/retrieve_all_forms_request.rb
+++ lib/create_api_gem/forms/requests/retrieve_all_forms_request.rb
@@ -19,13 +19,14 @@ require_relative 'form_request'
 require 'open-uri'
 
 class RetrieveAllFormsRequest < FormRequest
-  def initialize(token: APIConfig.token, forms_per_page: 10, page: nil, search: nil, theme: nil, workspace: nil)
+  def initialize(token: APIConfig.token, forms_per_page: 10, page: nil, search: nil, theme: nil, workspace: nil, from_id: nil)
     url = "#{APIConfig.api_request_url}/forms?"
     url << "page_size=#{forms_per_page}&" unless forms_per_page.nil?
     url << "page=#{page}&" unless page.nil?
     url << "search=#{URI.encode_www_form_component(search)}&" unless search.nil?
     url << "theme_id=#{theme.id}&" unless theme.nil?
     url << "workspace_id=#{workspace.id}&" unless workspace.nil?
+    url << "from_id=#{from_id}&" unless from_id.nil?
     r = {
       method: :get,
       url: url
