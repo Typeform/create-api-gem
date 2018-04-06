277da759ff313c598b0b15b9c53127e56838ff4f
simondobson <simon.dobson@typeform.com>
2018-04-06 16:10:25 +0200

    Remove redundant request

--- test/test_workspaces.rb
+++ test/test_workspaces.rb
@@ -36,9 +36,6 @@ class WorkspacesTest < TestBase
     assert_equal retrieve_default_workspace.success?, true
     default_workspace = retrieve_default_workspace.workspace
 
-    retrieve_workspace_forms = RetrieveWorkspaceFormsRequest.new(default_workspace)
-    assert_equal retrieve_workspace_forms.success?, true
-
     form = CreateFormRequest.execute(Form.new).form
     operations = [
       PatchOperation.new(op: 'replace', path: '/name', value: DataGenerator.title),
