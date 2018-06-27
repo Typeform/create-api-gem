30054ff68c190c5c5b57bc7f58457b75d892d7a7
Nicola Aitken <nicola@typeform.com>
2018-06-27 13:06:08 +0200

    [CR1-2371] Add Google Tag Manager to form settings

--- lib/create_api_gem/forms/settings/settings.rb
+++ lib/create_api_gem/forms/settings/settings.rb
@@ -18,11 +18,14 @@
 class Settings
   attr_accessor :redirect_after_submit_url, :show_typeform_branding, :progress_bar,
                 :show_progress_bar, :description, :allow_indexing, :image, :language,
-                :is_public, :is_trial, :google_analytics, :facebook_pixel, :notifications
+                :is_public, :is_trial, :google_analytics, :facebook_pixel, :google_tag_manager,
+                :notifications
 
   def initialize(redirect_after_submit_url: nil, show_typeform_branding: nil, progress_bar: nil,
                  show_progress_bar: nil, description: nil, allow_indexing: nil, image: nil, language: nil,
-                 is_public: nil, is_trial: nil, google_analytics: nil, facebook_pixel: nil, notifications: nil)
+                 is_public: nil, is_trial: nil, google_analytics: nil, facebook_pixel: nil,
+                 google_tag_manager: nil, notifications: nil)
+
     @redirect_after_submit_url = redirect_after_submit_url
     @show_typeform_branding = show_typeform_branding
     @progress_bar = progress_bar
@@ -35,6 +38,7 @@ class Settings
     @image = image
     @google_analytics = google_analytics
     @facebook_pixel = facebook_pixel
+    @google_tag_manager = google_tag_manager
     @notifications = notifications
   end
 
@@ -56,6 +60,7 @@ class Settings
     payload[:is_public] = is_public unless is_public.nil?
     payload[:google_analytics] = google_analytics unless google_analytics.nil?
     payload[:facebook_pixel] = facebook_pixel unless facebook_pixel.nil?
+    payload[:google_tag_manager] = google_tag_manager unless google_tag_manager.nil?
     payload[:notifications] = notifications.payload unless notifications.nil?
     unless description.nil? && allow_indexing.nil?
       payload[:meta] = {}
@@ -70,6 +75,7 @@ class Settings
     (redirect_after_submit_url.nil? || redirect_after_submit_url == actual.redirect_after_submit_url) &&
       (google_analytics.nil? || google_analytics == actual.google_analytics) &&
       (facebook_pixel.nil? || facebook_pixel == actual.facebook_pixel) &&
+      (google_tag_manager.nil? || google_tag_manager == actual.google_tag_manager) &&
       (notifications.nil? || notifications.same?(actual.notifications)) &&
       (description.nil? || description == actual.description) &&
       (show_typeform_branding.nil? ? Settings.default.show_typeform_branding : show_typeform_branding) == actual.show_typeform_branding &&
@@ -93,6 +99,7 @@ class Settings
     image = { href: APIConfig.image_api_request_url + '/images/default' }
     Settings.new(redirect_after_submit_url: 'http://google.com', show_typeform_branding: false, progress_bar: 'percentage',
                  show_progress_bar: false, description: 'some meta description', allow_indexing: true, image: image,
-                 language: 'fr', is_public: true, google_analytics: 'UA-1234-12', facebook_pixel: '12345678901234567', notifications: Notifications.full_example(email_block_for_notifications_ref))
+                 language: 'fr', is_public: true, google_analytics: 'UA-1234-12', facebook_pixel: '12345678901234567',
+                 google_tag_manager: 'GTM-PLWP6TS', notifications: Notifications.full_example(email_block_for_notifications_ref))
   end
 end
